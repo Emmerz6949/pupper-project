94952515d61c4e5ad575a30225f35f94fbc780b3
kelbri10 <kelbri10@gmail.com>
2020-10-29 20:52:51 -0400

    added route paths to index.js file so each page has own path name

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import CreateWalkerProfile from './pages/CreateWalkerProfile';
+//import CreateWalkerProfile from './pages/CreateWalkerProfile';
 import WalkerProfile from './pages/WalkerProfile.js';
 import DogProfile from './pages/DogProfile.js';
+import DogSchedule from './pages/DogSchedule.js'; 
+import CreateDogProfile from './pages/CreateDogProfile.js'; 
 
 
 function App() {
   return (
     <div className="App">
      
-        <WalkerProfile />
-        <DogProfile/>
-
+        <CreateDogProfile />
+        
         
      
     </div>
