f69a51c42eb12432d173004cab4081756a296312
kelbri10 <kelbri10@gmail.com>
2020-10-29 21:20:08 -0400

    reformatted login and sign up paths in index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-//import logo from './logo.svg';
+
 import './App.css';
-//import CreateWalkerProfile from './pages/CreateWalkerProfile';
-import WalkerProfile from './pages/WalkerProfile.js';
-import DogProfile from './pages/DogProfile.js';
-import DogSchedule from './pages/DogSchedule.js'; 
-import CreateDogProfile from './pages/CreateDogProfile.js'; 
+import Login from '../src/pages/login'; 
 
 
 function App() {
   return (
     <div className="App">
      
-        <CreateDogProfile />
+        <Login />
         
         
      
